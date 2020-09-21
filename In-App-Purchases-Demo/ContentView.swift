ed0d1e16ebb1295c9b67d17b0e1b8284464575a1
Carlos A Rodriguez <crodri81@gmail.com>
2020-09-21 12:20:02 -0300

    Initial Commit

---
+++ In-App-Purchases-Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  In-App-Purchases-Demo
+//
+//  Created by Carlos Rodriguez on 9/21/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
